07c82ec282a054720b973bec85ff58a6bc6983bc
pixeljogos <pixeljogoscontato@gmail.com>
2017-04-23 17:14:39 -0300

    Create telacheia.js

---
+++ telacheia.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	$('.switch').on('click', function(e) {
+$('#fulljogo').toggleClass('jogo jogofull');
+$('#telapequena').toggleClass('switch switch2');
+});
+$('.switch2').on('click', function(e) {
+$('#fulljogo').toggleClass('jogofull jogo');
+$('#telapequena').toggleClass('switch2 switch');
+});
+};
