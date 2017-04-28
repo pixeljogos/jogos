51f62a797f0de9024f3ca746f4fff09500de1036
pixeljogos <pixeljogoscontato@gmail.com>
2017-04-28 12:16:29 -0300

    Update telacheia.js

--- telacheia.js
+++ telacheia.js
@@ -6,3 +6,23 @@ $('.switch2').on('click', function(e) {
 $('#fulljogo').toggleClass('jogofull jogo');
 $('#telapequena').toggleClass('switch2 switch');
 });
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+};
